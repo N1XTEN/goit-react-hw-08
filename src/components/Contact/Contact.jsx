cc0b27a95a50b29832c269021f9ab4a09877513f
N1XTEN <130859559+N1XTEN@users.noreply.github.com>
2024-05-19 01:49:38 +0300

    fix contact

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,50 +1,67 @@
 import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { deleteContact, updateContact } from '../../redux/contacts/operations';
+import { ModalDelete } from './ModalDelete';
 import PropTypes from 'prop-types';
-import { BiSolidEditAlt } from 'react-icons/bi';
-import { RiDeleteBin6Line } from 'react-icons/ri';
+import { RiDeleteBin6Line } from "react-icons/ri";
+import { BiSolidEditAlt } from "react-icons/bi";
+import { ModalEdit } from './ModalEdit';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import css from './Contact.module.css';
-import ModalDelete from '../ModalDelete/ModalDelete';
-import ModalEdit from '../ModalEdit/ModalEdit';
 
-const Contact = ({ contact }) => {
+export const Contact = ({ contact }) => {
+  const dispatch = useDispatch();
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [isEditModalOpen, setIsEditModalOpen] = useState(false);
   const [editedContact, setEditedContact] = useState(contact);
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setEditedContact(prevContact => ({
-      ...prevContact,
-      [name]: value,
-    }));
+  const handleDelete = () => {
+    dispatch(deleteContact(contact.id))
+      .then(() => {
+        Notify.success('Contact successfully deleted');
+        setIsModalOpen(false);
+      })
+      .catch((error) => {
+        Notify.error('Error deleting contact')
+        console.error(error);
+      });
   };
 
   const handleEdit = () => {
-    
-    setIsEditModalOpen(false);
-  };
+  dispatch(updateContact({ id: editedContact.id, name: editedContact.name, number: editedContact.number }))
+    .then(() => {
+      Notify.success('Contact updated successfully');
+      setIsEditModalOpen(false);
+    })
+    .catch((error) => {
+      Notify.error('Error updating contact');
+      console.error(error);
+    });
+};
 
-  const handleDelete = () => {
-    
-    setIsModalOpen(false);
-  };
+
+const handleChange = (e) => {
+  const { name, value } = e.target;
+  setEditedContact({ ...editedContact, [name]: value });
+};
+
 
   return (
     <li className={css.list}>
       <p className={css.text}>{contact.name}</p>
       <p className={css.text}>{contact.number}</p>
       <div className={css.wrap}>
-        <button type="button" className={css.btn} onClick={() => setIsEditModalOpen(true)}>
+        <button type='button' className={css.btn} onClick={() => setIsEditModalOpen(true)}>
           <BiSolidEditAlt className={css.icon} />
         </button>
-        <button type="button" className={css.btn} onClick={() => setIsModalOpen(true)}>
+        <button type="button" onClick={() => setIsModalOpen(true)} className={css.btn}>
           <RiDeleteBin6Line className={css.icon} />
         </button>
       </div>
       <ModalDelete
         isOpen={isModalOpen}
         onRequestClose={() => setIsModalOpen(false)}
-        onDelete={handleDelete}
+        onDelete={handleDelete} 
       />
       <ModalEdit
         isOpen={isEditModalOpen}
@@ -58,10 +75,5 @@ const Contact = ({ contact }) => {
 };
 
 Contact.propTypes = {
-  contact: PropTypes.shape({
-    name: PropTypes.string.isRequired,
-    number: PropTypes.string.isRequired,
-  }).isRequired,
+  contact: PropTypes.object.isRequired, 
 };
-
-export default Contact;
